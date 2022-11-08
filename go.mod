199148b60dfb361d12b0431482b78097c7468b77
Karitham <kar@karitham.dev>
2022-11-08 18:42:55 +0100

    better logger and cli

--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module github.com/Karitham/proxyutil
 go 1.19
 
 require (
-	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi/v5 v5.0.7
 	github.com/sirupsen/logrus v1.9.0
-	github.com/spf13/cobra v1.6.1
+	github.com/urfave/cli/v2 v2.23.4
 )
 
 require (
-	github.com/inconshreveable/mousetrap v1.0.1 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 	"strings"
+	"time"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
+	"github.com/urfave/cli/v2"
+
+	stdlog "log"
 )
 
 type Proxy struct {
@@ -18,19 +23,51 @@ type Proxy struct {
 	URI     *url.URL
 }
 
+var log = logrus.New()
+
 func main() {
 	port := 8080
+	level := uint(2)
 
-	root := &cobra.Command{
-		Short: "proxyutil is a simple proxy server",
-		Args:  cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			proxies := make([]Proxy, 0, len(args)-1)
+	app := &cli.App{
+		Name:  "proxyutil",
+		Usage: "proxyutil is a simple proxy server",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:        "port",
+				Aliases:     []string{"p"},
+				EnvVars:     []string{"PROXYUTIL_PORT"},
+				Value:       8080,
+				Usage:       "port to listen on",
+				Destination: &port,
+			},
+			&cli.UintFlag{
+				Name:        "verbose",
+				Aliases:     []string{"v"},
+				EnvVars:     []string{"PROXYUTIL_VERBOSE"},
+				Value:       4,
+				Usage:       "enable verbose logging. 0 = panic, 1 = fatal, 2 = error, 3 = warn, 4 = info, 5 = debug, 6 = trace",
+				Destination: &level,
+			},
+		},
+
+		Before: func(ctx *cli.Context) error {
+			log.SetLevel(logrus.Level(level))
+			return nil
+		},
+
+		Action: func(c *cli.Context) error {
+			if c.NArg() == 0 {
+				return fmt.Errorf("no proxy specified")
+			}
+
+			args := c.Args().Slice()
+			proxies := make([]Proxy, 0, len(args))
 
 			for _, arg := range args {
 				proxy, err := parseProxy(arg)
 				if err != nil {
-					logrus.Fatalln(err)
+					log.Fatalln(err)
 				}
 				proxies = append(proxies, proxy)
 			}
@@ -38,36 +75,31 @@ func main() {
 			for i := range proxies {
 				proxy := proxies[i]
 
-				logrus.Infof("Proxying: %s -> %s", proxy.SubPath, proxy.URI)
+				log.Infof("Proxying: %s -> %s", proxy.SubPath, proxy.URI)
 			}
 
 			r := chi.NewRouter()
-			r.Use(middleware.Logger)
+			r.Use(reqLogger(log))
+
 			for i := range proxies {
-				proxy := proxies[i]
+				proxy := httputil.NewSingleHostReverseProxy(proxies[i].URI)
 
-				r.Mount(proxy.SubPath, proxyHandler(proxy))
+				// logger
+				lg := &stdlog.Logger{}
+				lg.SetOutput(log.WithField("path", proxies[i].SubPath).WriterLevel(logrus.WarnLevel))
+				proxy.ErrorLog = lg
+
+				r.Mount(proxies[i].SubPath, proxy)
 			}
 
-			PORT := fmt.Sprintf(":%d", port)
+			addr := fmt.Sprintf(":%d", port)
 
-			logrus.Infof("Listening on %s", PORT)
-			err := http.ListenAndServe(PORT, r)
-			if err != nil {
-				logrus.Fatalln(err)
-			}
+			log.Infof("Listening on %s", addr)
+			return http.ListenAndServe(addr, r)
 		},
 	}
 
-	root.Flags().IntVarP(&port, "port", "p", port, "port to listen on")
-	logrus.Fatal(root.Execute())
-}
-
-func proxyHandler(proxy Proxy) http.Handler {
-	proxyH := httputil.NewSingleHostReverseProxy(proxy.URI)
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		proxyH.ServeHTTP(w, r)
-	})
+	log.Fatal(app.Run(os.Args))
 }
 
 func parseProxy(arg string) (Proxy, error) {
@@ -85,4 +117,40 @@ func parseProxy(arg string) (Proxy, error) {
 		SubPath: subP,
 		URI:     u,
 	}, nil
+}
+
+// reqLogger returns a request logging middleware
+func reqLogger(logger logrus.FieldLogger) func(h http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			reqID := middleware.GetReqID(r.Context())
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+			t1 := time.Now()
+			defer func() {
+				remoteIP, _, err := net.SplitHostPort(r.RemoteAddr)
+				if err != nil {
+					remoteIP = r.RemoteAddr
+				}
+				scheme := "http"
+				if r.TLS != nil {
+					scheme = "https"
+				}
+				fields := logrus.Fields{
+					"status_code": ww.Status(),
+					"bytes":       ww.BytesWritten(),
+					"duration":    time.Since(t1).String(),
+					"remote_ip":   remoteIP,
+					"method":      r.Method,
+				}
+				if len(reqID) > 0 {
+					fields["request_id"] = reqID
+				}
+				logger.WithFields(fields).Debugf("%s://%s%s", scheme, r.Host, r.RequestURI)
+			}()
+
+			h.ServeHTTP(ww, r)
+		}
+
+		return http.HandlerFunc(fn)
+	}
 }
